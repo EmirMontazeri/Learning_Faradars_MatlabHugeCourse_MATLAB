d61dde009894d2c8824de01c62ce074f4ae62445
Amirmohammad Montazeri <137637411+EmirMontazeri@users.noreply.github.com>
2026-07-29 10:15:06 +0900

    Add files from Session 3

---
+++ Matlab_Programming/S3/Divider.m
@@ -0,0 +1,12 @@
+function DL = Divider(n)
+    
+    DL = [1, n];
+    for i = 2:floor(sqrt(n))
+        if mod(n,i)==0
+            DL = [DL, i, n/i]; 
+        end
+    end
+    
+    DL = unique(DL);
+
+end
